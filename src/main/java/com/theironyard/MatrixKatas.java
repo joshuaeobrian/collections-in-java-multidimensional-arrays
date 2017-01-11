3a7adff685e68e1bafa8d2eb16d6dd3249b94a2a
Doug Hughes <doug@theironyard.com>
2017-01-11 16:58:21 -0500

    add exercise for multidimensional arrays

---
+++ src/main/java/com/theironyard/MatrixKatas.java
@@ -0,0 +1,34 @@
+package com.theironyard;
+
+/**
+ * Implement this class' methods as instructed below
+ */
+public class MatrixKatas {
+
+    /**
+     * Create a static method named createProductTable(). It should accept two
+     * integer arguments representing the size of a two dimensional array. The
+     * method will create an array of that size and populate it with the
+     * associated products.
+     *
+     * For example, arguments of 4, 3 would produce this matrix:
+     *
+     * [
+     *  [0, 0, 0],
+     *  [0, 1, 2],
+     *  [0, 2, 4],
+     *  [0, 3, 6]
+     * ]
+     *
+     * Note that the outer array has four elements. Each inner array has three
+     * elements. Let's call this array `products`. If we were to access
+     * products[3,2] we would find 6.
+     *
+     * This method returns an array of ints.
+     *
+     * @param a The size of the "outer" array of arrays of ints.
+     * @param b The size of the "inner" arrays of ints.
+     * @return A two dimensional array of integers where the value is the product of the array indexes
+     */
+    // todo: create createProductTable() method
+}
---
+++ src/test/java/com/theironyard/MatrixKatasTest.java
@@ -0,0 +1,76 @@
+package com.theironyard;
+
+import net.doughughes.testifier.exception.CannotAccessMethodException;
+import net.doughughes.testifier.exception.CannotFindMethodException;
+import net.doughughes.testifier.exception.CannotInvokeMethodException;
+import net.doughughes.testifier.matcher.RegexMatcher;
+import net.doughughes.testifier.test.TestifierTest;
+import net.doughughes.testifier.util.Invoker;
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class MatrixKatasTest extends TestifierTest{
+
+    @Test
+    public void testCreateProductTable3By5() {
+        /* arrange */
+
+        /* act */
+        int[][] result = new int[0][];
+        try {
+            result = (int[][]) Invoker.invokeStatic(MatrixKatas.class, "createProductTable", 3, 5);
+        } catch (CannotFindMethodException | CannotAccessMethodException | CannotInvokeMethodException e) {
+            fail(e.getMessage());
+        }
+
+        /* assert */
+        assertThat("The array that contains arrays of ints should have a length of 3",
+                result.length, equalTo(3));
+        for(int[] set : result){
+            assertThat("The arrays of ints contained in the array a length of 5",
+                    set.length, equalTo(5));
+        }
+    }
+
+    @Test
+    public void testCreateProductTable3By4Values() {
+        /* arrange */
+
+        /* act */
+        int[][] result = new int[0][];
+        try {
+            result = (int[][]) Invoker.invokeStatic(MatrixKatas.class, "createProductTable", 3, 4);
+        } catch (CannotFindMethodException | CannotAccessMethodException | CannotInvokeMethodException e) {
+            fail(e.getMessage());
+        }
+
+        /* assert */
+        for(int x = 0 ; x < 3 ; x++){
+            for(int y = 0 ; y < 4 ; y++){
+                assertThat(x + " * " + y + " should equal " + (x * y),
+                        result[x][y], equalTo(x * y));
+            }
+        }
+
+    }
+
+    @Test
+    public void testCreateProductTableStructure(){
+        /* arrange */
+
+        /* act */
+        String source = null;
+        try {
+            source = codeWatcher.getMainSourceCodeService().getDescriptionOfMethod("createProductTable", int.class, int.class);
+        } catch (CannotFindMethodException e) {
+            fail(e.getMessage());
+        }
+
+        /* assert */
+        assertThat("The createProductTable() method should contain nested for loops.",
+                source, RegexMatcher.matches("^.*?ForStmt.*?BlockStmt.*?ForStmt.*?BlockStmt.*?/BlockStmt.*?/ForStmt.*?/BlockStmt.*?/ForStmt.*?$"));
+    }
+}
